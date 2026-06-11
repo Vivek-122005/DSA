9bfd2e057cc169bff0b5dc57821dacaf4ad5a972
Vivek <vivek.23csai@nst.rishihood.edu.in>
2026-06-11 14:37:06 +0530

    Time: 15 ms (99.45%) | Memory: 19.3 MB (85.71%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -4,12 +4,19 @@ class Solution:
         N = len(nums)
         data = []
 
-        for i in range (0,N):
+        for i in range (0,N-3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for j in range(i+1,N):
+
+            if nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target: break
+            if nums[i] + nums[N-1] + nums[N-2] + nums[N-3] < target: continue
+
+            for j in range(i+1,N-2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
+                if nums[i] + nums[j] + nums[j+1]+nums[j+2] > target : break
+                if nums[i] + nums[j] + nums[N-1]+nums[N-2] < target : continue
+
                 start = j+1
                 end = N-1
                 
