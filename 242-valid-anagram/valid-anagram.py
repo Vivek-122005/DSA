7b7542146d49ac20b7cf785e5bfcdf0cacc06806
Vivek <vivek.23csai@nst.rishihood.edu.in>
2026-06-12 23:14:34 +0530

    Time: 3 ms (98.36%) | Memory: 19.4 MB (44.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        seen = {}
-        see = {}
         if len(s) != len(t):
             return False
-        for num in s:
-            if num not in seen:
-                seen[num] = 0
-            seen[num] += 1
-        for nu in t:
-            if nu not in see:
-                see[nu] = 0
-            see[nu] += 1
-        return seen == see
 
+        s_counter = Counter(s)
+        t_counter = Counter(t)
+        return s_counter == t_counter
+                
             
 
         
