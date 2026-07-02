a9d508e560aea23536e47d79c7daa8a10e20d34f
Vivek <vivek.23csai@nst.rishihood.edu.in>
2026-07-02 09:09:15 +0530

    Time: 10 ms (59.83%) | Memory: 23.2 MB (17.03%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,37 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        import heapq
-        # intitalize heap. 
-        # heap item tuple = (value, )
-        heap = []
-        counter = 0
-        merged = ListNode()
-        curr = merged
-        for li in range(len(lists)):
-            if lists[li] == None:
-                continue
-            counter +=1 
-            heapq.heappush(heap,(lists[li].val, counter, lists[li]) )
-            
+        pq = []
 
-        # now we take out one element from the heap and store it in new list, and do this till heap becomes empty
-        # counter is just for doing tie breaks for cases where velues of items in heap are same. since they increase everytime a new item is added to heap, it is unique. 
-        while not len(heap)==0:
-            item = heapq.heappop(heap)
-            if item[2] != None:
-                curr.next = item[2]
-                curr = curr.next
-                if item[2].next != None:
-                    counter +=1 
-                    heapq.heappush(heap, (item[2].next.val,counter,item[2].next))
-                else:
-                    # actually nothing is required to be done. 
-                    continue
-            else:
-                pass
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(pq,(lists[i].val,i,lists[i]))
 
-        merged = merged.next
-        return merged
+        dummynode = ListNode()
+        temp = dummynode
+        while pq:
+            val , i , node = heapq.heappop(pq)
+            temp.next = node
+            temp = temp.next
+
+            if node.next:
+                heapq.heappush(pq,(node.next.val,i, node.next))
+        return dummynode.next
+
+        
