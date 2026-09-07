b40dcbadc6f44ab7348dbc638df7dbbb089c451a
Vivek <vivek.23csai@nst.rishihood.edu.in>
2026-09-07 13:48:23 +0530

    Time: 882 ms (9.77%) | Memory: 100.4 MB (22.60%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,10 +1,26 @@
 class Solution:
+    def solve(self, s: str, t: str, m: int, n:int,memo) -> int:
+        if n ==0:
+            return 1
+        if m == 0:
+            return 0
+        if (m,n) in memo:
+            return memo[(m,n)]
+        
+        if s[m-1] ==t[n-1]:
+            take = self.solve(s,t,m-1,n-1,memo)
+            slip = self.solve(s,t,m-1,n,memo)
+            memo[(m,n)] =  take + slip
+        else:
+            memo[(m,n)]= self.solve(s,t,m-1,n,memo)
+
+        return memo[(m,n)]
     def numDistinct(self, s: str, t: str) -> int:
-        n, m = len(s), len(t)
-        dp = [0] * (m + 1)
-        dp[0] = 1
-        for i in range(1, n + 1):
-            for j in range(m, 0, -1):
-                if s[i-1] == t[j-1]:
-                    dp[j] += dp[j-1]
-        return dp[m]
+        m = len(s)
+        n = len(t)
+        if m < n:
+            return 0
+        memo = {}
+
+        return self.solve(s,t,m,n, memo)
+        
